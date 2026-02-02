ab52f1446bc27f7f9fe8b7cfc95da924f97e3628
sportyskater69 <rowan.feland@edu.sait.ca>
2026-02-02 11:49:18 -0700

    Header started

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,11 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Headerrow from "./header";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Headerrow />
     </View>
   );
 }
