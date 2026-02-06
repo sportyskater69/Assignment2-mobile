4196d53bc533e29189017d1971ee3036c28874e7
zeynepcamgoz <Zeynep.Camgoz@edu.sait.ca>
2026-02-06 10:15:43 -0700

    Bottom Icons added

--- app/buttons.tsx
+++ app/buttons.tsx
@@ -10,31 +10,31 @@ export default function BottomButtons() {
       }}>
         <Pressable onPress={() => { }}>
           <Image
-            source={require("../assets/images/icon.png")}
+            source={require("../assets/images/Home-Icon.png")}
             style={{ width: 30, height: 30 }}
           />
         </Pressable>
         <Pressable onPress={() => { }}>
           <Image
-            source={require("../assets/images/icon.png")}
+            source={require("../assets/images/Search-Icon.png")}
             style={{ width: 30, height: 30 }}
           />
         </Pressable>
         <Pressable onPress={() => { }}>
           <Image
-            source={require("../assets/images/icon.png")}
+            source={require("../assets/images/Reel-Icon.png")}
             style={{ width: 30, height: 30 }}
           />
         </Pressable>
         <Pressable onPress={() => { }}>
           <Image
-            source={require("../assets/images/icon.png")}
+            source={require("../assets/images/Shop-Icon.png")}
             style={{ width: 30, height: 30 }}
           />
         </Pressable>
         <Pressable onPress={() => { }}>
           <Image
-            source={require("../assets/images/icon.png")}
+            source={require("../assets/images/Profile-Icon.png")}
             style={{ width: 30, height: 30 }}
           />
         </Pressable>
